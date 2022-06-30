c1139d9883e3a2251badb63920f02493855f6cba
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-06-30 19:04:11 +0200

    dispatch explanation

--- example.js
+++ example.js
@@ -9,6 +9,13 @@ function reducer(state, action) {
 }
 
 function dispatch(action) {
+
+    if (typeof action === 'function') {
+        action(dispatch)
+    } else {
+        store.push(action)
+    }
+
     store = reducer(store, action);
 }
 
@@ -20,4 +27,45 @@ const action1 = {
 dispatch(action1);
 dispatch(action1);
 dispatch(action1); // store is now [{ answer: 42 }, { answer: 42 }, { answer: 42 }]
+////////////////////
+// to jest to samo co niżej
+dispatch ((dispatch_) =>{
+    dispatch_({
+        type: MAGIC,
+        payload: {magic: false}
+    })
+})
 console.log(store);
+
+// to jest to samo co wyżej 
+const doMagic = (dispatch_) =>{
+    dispatch_({
+        type: MAGIC,
+        payload: {magic: false}
+    })
+}
+dispatch (doMagic)
+console.log(store);
+////////////////////
+
+const upper = (text) => {
+    return text[0].toUpperCase() + text.slice[1];
+}
+
+const reverse = (text) => {
+    return text.split('').reverse().join('');
+}
+
+const createSentece = (text, fn) => {
+    return fn(text);
+}
+
+// console.log(createSentece(upper('Elo'), reverse));
+console.log(createSentece('Elo', upper, reverse));
+
+
+// function notThunk() {
+//     return function thunk (){
+        
+//     }
+// }
