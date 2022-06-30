ba9846b64deb2a0b85bd3e13daf793c08027e817
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-06-30 18:31:25 +0200

    example js add

---
+++ src/components/example.js
@@ -0,0 +1,20 @@
+let store = []
+const MAGIC = 'Magic';
+
+function reducer(state, action) {
+    if (action.type === MAGIC) {
+        return [...state, action.payload]
+    }
+    return state
+}
+
+function dispatch(action) {
+    store = reducer(store, action);
+}
+
+const action1 = {
+    type: MAGIC,
+    payload: { answer: 42 }
+}
+
+dispatch(action1);
